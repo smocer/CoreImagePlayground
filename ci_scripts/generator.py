72d51441cd9fc048584526151b1cf43d46e6e160
Egor Butyrin <egorbutyrin@mynalabs.com>
2022-12-07 12:23:00 +0700

    test

--- ci_scripts/generator.py
+++ ci_scripts/generator.py
@@ -4,7 +4,7 @@ import sys
 with open("test.json", encoding="utf-8") as json_file:
     data = json.load(json_file)
 with open("test.json", "w", encoding="utf-8") as json_file:
-    data["test"] = "😂 json test wéird symbôls!"
+    data["testTo"] = data["testFrom"]
     json_file.write(json.dumps(data, ensure_ascii=False, sort_keys=False, indent=2, separators=(',', ': ')) + '\n')
 
 sys.exit()
